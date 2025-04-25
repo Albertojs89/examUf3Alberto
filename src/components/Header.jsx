25d5d4406676d37dac28544ddd85f067b7609c4f
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2025-04-25 16:04:57 +0200

    react+rout + vistas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
-    <header className="bg-primary text-white p-3">
-      <h1>Nom: Alberto</h1>
+    <header className="bg-primary text-white mb-4">
+      <nav className="navbar navbar-expand-lg navbar-dark container">
+        <span className="navbar-brand">Alberto</span>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/usuarios" className="nav-link">Usuarios</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/camareros" className="nav-link">Camareros</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 }
